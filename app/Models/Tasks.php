7c64741597fa24036703945e11e6429e54871e47
poom <poom10510@gmail.com>
2018-04-28 20:43:44 +0700

    add leave task taskjoin model

--- app/Models/Tasks.php
+++ app/Models/Tasks.php
@@ -16,4 +16,34 @@ class Tasks extends Model
     protected $table = 'Tasks';
     protected $primaryKey = 'id';
     public $timestamps = false;
+
+    public function getdata(){
+        $table = $this->table;
+        $datas = DB::select("select * from $table");
+        return $datas;
+    }
+
+    public function insertdata($data)
+    {
+        $table = $this->table;
+        //
+        $title = $data['title'];
+        $description = $data['description'];
+        $userid = $data['userid'];
+        # code...
+        DB::insert("insert into $table (title, description, userid) values (?, ?, ?)", [ $title, $description, $userid]);
+    }
+
+    public function deletedata($id){
+        $table = $this->table;
+        DB::delete("delete from $table where id = ?",[$id]);
+    }
+
+    public function finddata($id){
+        $table = $this->table;
+        $datas = DB::select("select * from $table where id = ?", [$id]);
+        return $datas;
+    }
+
+
 }
--- app/Models/TasksJoin.php
+++ app/Models/TasksJoin.php
@@ -18,4 +18,30 @@ class TasksJoin extends Model
     protected $primaryKeyser = 'userid';
     public $timestamps = false;
 
+    public function getdata(){
+        $table = $this->table;
+        $datas = DB::select("select * from $table");
+        return $datas;
+    }
+
+    public function insertdata($data)
+    {
+        $table = $this->table;
+        //
+        $taskid = $data['taskid'];
+        $userid = $data['userid'];
+        # code...
+        DB::insert("insert into $table (taskid, userid) values (?, ?)", [ $taskid, $userid]);
+    }
+
+    public function deletedata($id){
+        $table = $this->table;
+        DB::delete("delete from $table where id = ?",[$id]);
+    }
+
+    public function finddata($id){
+        $table = $this->table;
+        $datas = DB::select("select * from $table where id = ?", [$id]);
+        return $datas;
+    }
 }
